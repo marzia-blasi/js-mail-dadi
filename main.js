0f4abd1c5fb60912ce9599bf4fc9b11a62848968
marzia <you@example.com>
2025-05-30 15:15:21 +0200

    Add initial HTML structure and JavaScript comments for email and dice game functionality

---
+++ main.js
@@ -0,0 +1,21 @@
+/* Mail
+1 Crea una lista di email di invitati ad una festa. 
+- array
+2 Chiedi all’utente la sua email, 
+- prompt
+3 controlla che sia nella lista di chi può accedere, 
+- servirà un === 
+4 stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. 
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).*/
+
+/* 
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare?
+torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" 
+ */
